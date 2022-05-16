ba60bf180f89469a6a2323f135b33210ef099ce7
cares0 <cares00000@gmail.com>
2022-05-16 21:37:50 +0900

    폼 전송객체 분리

--- src/main/java/hello/itemservice/web/validation/form/ItemUpdateForm.java
+++ src/main/java/hello/itemservice/web/validation/form/ItemUpdateForm.java
@@ -1,27 +1,26 @@
-package hello.itemservice.domain.item;
+package hello.itemservice.web.validation.form;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
-import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
-public class Item {
+public class ItemUpdateForm {
 
+    @NotNull
     private Long id;
+
+    @NotBlank
     private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
     private Integer price;
+
+    // 수정에서는 수량을 자유롭게 사용 가능
     private Integer quantity;
 
-    public Item() {
-    }
-
-    public Item(String itemName, Integer price, Integer quantity) {
-        this.itemName = itemName;
-        this.price = price;
-        this.quantity = quantity;
-    }
 }
